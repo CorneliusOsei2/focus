beddbdf6847ff745b1cfca727b55248fa2e07404
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-30 12:56:42 -0700

    ContentItem registered

--- focus/apps_home/views.py
+++ focus/apps_home/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ContentsItem
 
 # Create your views here.
 class HomeViews:
     def home_top(request):
-        return render(request, 'homeTop.html')
+        return render(request, 'home_top.html')
 
     def home_bottom(request):
-        return render(request, 'homeBottom.html')
+        return render(request, 'home_bottom.html')
--- focus/focus/settings.py
+++ focus/focus/settings.py
@@ -15,6 +15,7 @@ SO HELP US GOD!
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps_home',
+    'apps_users',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'focus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, "templates/html"],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
