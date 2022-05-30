b88b09e6e690e714bb2cb8e979c4b864a120f51f
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-30 01:43:01 -0700

    User component and url added

--- focus/focus/urls.py
+++ focus/focus/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("apps_home.urls"))
+    path('', include("apps_home.urls")),
+     path('users/', include("apps_users.urls"))
 ]
