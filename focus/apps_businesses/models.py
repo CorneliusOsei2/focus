1c5b6a08f4aa83f5e532585386f9b34a5930a3fc
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-31 09:42:23 -0700

    Business and User models started

---
+++ focus/apps_businesses/models.py
@@ -0,0 +1,12 @@
+from django.db.models import Model, CharField, IntegerField
+
+# Create your models here.
+class Business(Model):
+    title = CharField(max_length=100, default="")
+    number = IntegerField(default=1)
+    description = CharField(max_length=100, default="")
+    growth_rate = IntegerField(default=100)
+    '''
+    business -> investors
+    business -> sales
+    '''
