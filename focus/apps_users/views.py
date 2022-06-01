f8fa120160ab572953b29dae074b5404c70d6f9c
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-31 20:32:34 -0700

    Register page created

--- focus/apps_users/views.py
+++ focus/apps_users/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class UserViews:
-    def login(request):
-        
-        return HttpResponse("Hiop")
+def register(request):
+    """ Register a new user """
+    
+    return render(request, 'register.html')
+
+def login(request):
+    """ Log in a user """
+    return HttpResponse("Hiop")
