cf658bfe9e3870960718efba2f963fc35bd09c2c
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-06-02 09:28:23 -0700

    User model successfully created!

--- focus/apps_users/models.py
+++ focus/apps_users/models.py
@@ -1,18 +1,64 @@
-from django.db.models import Model, CharField, IntegerField
-
-# Create your models here.
-class User(Model):
-    first_name = CharField(max_length=100, default="")
-    last_name = CharField(max_length=100, default="")
-    username = CharField(max_length=100, default="")
-    dob = IntegerField()
-
-    '''
-    user -> investments
-    user -> sales
-
-    '''
+from django.db.models import CharField, EmailField, IntegerField, BooleanField
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 
 
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, first_name, last_name, username, contact, email, password, **other_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            contact=contact,
+            **other_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, first_name, last_name, username, contact, email, password, **other_fields):
 
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        user = self.create_user(
+            first_name,
+            last_name,
+            username,
+            contact,
+            email,
+            password,
+            **other_fields
+        )
+
+        return user
+
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    first_name = CharField(max_length = 50, blank=False)
+    last_name = CharField(max_length = 50, blank=False)
+    username = CharField(max_length = 50, unique = True)
+    contact = IntegerField(blank=False)
+    email = EmailField(_('email address'), unique = True, blank=False)
+    is_active= BooleanField(default=False)
+    is_staff= BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'contact', 'email']
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return "{}".format(self.email)
--- focus/apps_users/views.py
+++ focus/apps_users/views.py
@@ -1,13 +1,51 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def register(request):
     """ Register a new user """
-    
-    return render(request, 'register.html')
+    if (request.method == "POST"):
+        firstname, lastname, username = request.POST['firstname'], request.POST['lastname'], request.POST['username']
+        contact, email = request.POST['contact'], request.POST['email']
+        password, repeated_password = request.POST['password'], request.POST['repeatpassword']
+
+        if firstname is None:
+            messages.info(request, "Enter first name")
+            return redirect("register")
+
+        if password == repeated_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists!')
+                return redirect("register")
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists!')
+                return redirect("register")
+            if User.objects.filter(contact=contact).exists():
+                messages.info(request, 'Contact already exists!')
+                return redirect("register")
+
+            user = User.objects.create_user(
+                firstname=firstname,
+                lastname =lastname,
+                username=username,
+                contact=contact,
+                email=email,
+                password=password
+            )
+            user.save()
+            return redirect(request, "login")
+
+        else:
+            messages.info(request, "Passwords are not the same!")
+            return redirect("register")
+    elif (request.method == "GET"):
+        return render(request, 'register.html')
 
 def login(request):
     """ Log in a user """
-    return HttpResponse("Hiop")
+    
+    if (request.method == "POST"):
+        pass
