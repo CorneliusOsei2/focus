e44c7aaa174a5ec1d3c229a5bc6ed9c500eb7e3e
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-06-03 12:24:45 -0700

    Login page and route

--- focus/apps_users/views.py
+++ focus/apps_users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .models import CustomUser
-
+from django.contrib.auth import authenticate
 # Create your views here.
 
 def register(request):
@@ -18,13 +18,13 @@ def register(request):
 
         if password == repeated_password:
             if CustomUser.objects.filter(username=username).exists():
-                messages.info(request, 'Username already exists!')
+                messages.info(request, 'Username Already Exists!')
                 return redirect("register")
             if CustomUser.objects.filter(email=email).exists():
-                messages.info(request, 'Email already exists!')
+                messages.info(request, 'Email Already Exists!')
                 return redirect("register")
             if CustomUser.objects.filter(contact=contact).exists():
-                messages.info(request, 'Contact already exists!')
+                messages.info(request, 'Contact Already Exists!')
                 return redirect("register")
 
             new_user = CustomUser.objects.create_user(
@@ -35,11 +35,11 @@ def register(request):
                 email=email,
                 password=password
             )
-            return redirect(request, "login")
-
+            return redirect("login")
         else:
             messages.info(request, "Passwords are not the same!")
             return redirect("register")
+
     elif (request.method == "GET"):
         return render(request, 'register.html')
 
@@ -47,4 +47,12 @@ def login(request):
     """ Log in a user """
     
     if (request.method == "POST"):
-        pass
+        username, password = request.POST['username'], request.POST['password']
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                return HttpResponse("Done!")
+        return HttpResponse("sorry")
+    else:
+        return render(request, 'login.html')
