64b49402a7924d83947c01d98fde179bc4947a5d
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-06-05 17:35:02 -0700

    Login implemented

--- focus/apps_home/models.py
+++ focus/apps_home/models.py
@@ -5,8 +5,8 @@ class ContentsItem(Model):
     title =  CharField(max_length=100, null=False, default='')
     description = CharField(max_length=100, null=False, default='')
     destination = CharField(max_length=100, null=False, default='')
-    position = CharField(max_length=10, null=False, default='')
-    style = CharField(max_length=50, null=False, default='')
-   
+    style = CharField(max_length=100, null=False, default='')
+
+    
 
    
