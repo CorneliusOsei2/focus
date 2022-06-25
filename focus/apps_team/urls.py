6d331aa69fbc8fa8b712e2071715147e03b2f793
Cornelius Osei Owusu Boateng <78667952+CorneliusOsei2@users.noreply.github.com>
2022-06-25 14:47:27 -0700

    Delete focus directory

--- focus/apps_team/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import team, contact
-
-urlpatterns = [
-    path('team', team, name='team'),
-    path('contact', contact, name='contact')
-]
--- focus/apps_users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppsUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps_users'
