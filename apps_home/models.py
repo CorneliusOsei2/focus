b8e20ce1672f21569731ef4bdd7a0927c9dd9877
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-07-24 15:21:26 -0700

    Fix carousel

--- apps_home/models.py
+++ apps_home/models.py
@@ -10,7 +10,7 @@ class ContentsItem(Model):
 class Highlight(Model):
     title =  CharField(max_length=100, null=False, default='')
     imgLink = CharField(max_length=1000, null=False, default='')
-    style = CharField(max_length=100, null=False, default='')
+    style = CharField(max_length=50, default='', null=True)
     description = CharField(max_length=300, null=False, default='')
 
    
